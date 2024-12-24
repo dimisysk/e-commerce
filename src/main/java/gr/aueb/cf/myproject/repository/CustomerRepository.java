47a8b6f642ab89c3619c86c203473d26c30b3a7d
Dimi Syskakis <Syskakisdimitrios@yahoo.com>
2024-12-24 03:13:06 +0100

    Repository

--- src/main/java/gr/aueb/cf/myproject/repository/CustomerRepository.java
+++ src/main/java/gr/aueb/cf/myproject/repository/CustomerRepository.java
@@ -1,10 +1,16 @@
 package gr.aueb.cf.myproject.repository;
 
+import ch.qos.logback.core.model.INamedModel;
 import gr.aueb.cf.myproject.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long>, JpaSpecificationExecutor<Customer> {
+
+
+    Optional<Customer> findByDiscountCardNumber(Integer discountCardNumber);
 }
