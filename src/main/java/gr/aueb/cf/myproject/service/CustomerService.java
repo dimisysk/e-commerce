671f21bc53c38d6ccfd33526ecc683eddb849563
Dimi Syskakis <Syskakisdimitrios@yahoo.com>
2024-12-25 02:49:21 +0100

    customer service

--- src/main/java/gr/aueb/cf/myproject/service/CustomerService.java
+++ src/main/java/gr/aueb/cf/myproject/service/CustomerService.java
@@ -7,14 +7,20 @@ import gr.aueb.cf.myproject.dto.CustomerReadOnlyDTO;
 import gr.aueb.cf.myproject.mapper.CustomerAdminMapper;
 import gr.aueb.cf.myproject.mapper.EntityToDtoMapper;
 import gr.aueb.cf.myproject.model.Customer;
+import gr.aueb.cf.myproject.repository.AdminRepository;
 import gr.aueb.cf.myproject.repository.CustomerRepository;
 import gr.aueb.cf.myproject.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
+
 
     private CustomerRepository customerRepository;
     private UserRepository userRepository;
@@ -25,19 +31,21 @@ public class CustomerService {
     public CustomerService(CustomerRepository customerRepository,
                            UserRepository userRepository,
                            CustomerAdminMapper customerAdminMapper,
-                           EntityToDtoMapper entityToDtoMapper
-    )
+                           EntityToDtoMapper entityToDtoMapper,
+                           AdminRepository adminRepository)
     {
         this.customerRepository = customerRepository;
         this.userRepository = userRepository;
         this.customerAdminMapper = customerAdminMapper;
         this.entityToDtoMapper = entityToDtoMapper;
+
     }
 
+    @Transactional(rollbackOn = Exception.class)
     public CustomerReadOnlyDTO saveCustomer(CustomerInsertDTO customerInsertDTO) throws AppObjectAlreadyExists {
 
         if (userRepository.findByUsername(customerInsertDTO.getUser().getUsername()).isPresent()) {
-            throw new AppObjectAlreadyExists("User", "User with Username:" + customerInsertDTO.getUser().getUsername() + "already exists");
+            throw new AppObjectAlreadyExists("Customer", "User with Username:" + customerInsertDTO.getUser().getUsername() + "already exists");
         }
         if (customerRepository.findByDiscountCardNumber(customerInsertDTO.getDiscountCardNumber()).isPresent()) {
             throw new AppObjectAlreadyExists("DiscountCardNumber", "Discount Card with the Number" +customerInsertDTO.getDiscountCardNumber() + "already exists");
@@ -49,6 +57,22 @@ public class CustomerService {
 
         return entityToDtoMapper.mapToCustomerReadOnlyDTO(savedCustomer);
     }
+
+    public Page<CustomerReadOnlyDTO> getPaginatedCustomers(int page, int size, String sortBy) {
+      String defaultSort = "id";
+        Pageable pageable = PageRequest.of(page, size, Sort.by(defaultSort).ascending());
+        return customerRepository.findAll(pageable).map(entityToDtoMapper::mapToCustomerReadOnlyDTO);
+    }
+
+    public Page<CustomerReadOnlyDTO> getSortedCustomers(int page, int size, String sortBy, String sortDirection) {
+        Sort sort = Sort.by(Sort.Direction.fromString(sortDirection), sortBy);
+        Pageable pageable = PageRequest.of(page,size, sort);
+        return customerRepository.findAll(pageable).map(entityToDtoMapper::mapToCustomerReadOnlyDTO);
+    }
+
+
+
+
 
 
 }
