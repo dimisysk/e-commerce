ea0bf9a90263e68f0d0a019813a8d721d9691167
Dimi Syskakis <Syskakisdimitrios@yahoo.com>
2024-12-26 02:01:28 +0100

    Admin Repository

--- src/main/java/gr/aueb/cf/myproject/repository/AdminRepository.java
+++ src/main/java/gr/aueb/cf/myproject/repository/AdminRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 public interface AdminRepository extends JpaRepository<Admin, Long>, JpaSpecificationExecutor<Admin> {
 
     Optional<Admin> findByUserId(Long id);
-    Optional<Admin> findByUuid(Long id);
+    Optional<Admin> findByUuid(String id);
 }
