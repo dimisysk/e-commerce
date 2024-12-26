d692819184254d521167c106976ded6022d4ddd0
Dimi Syskakis <Syskakisdimitrios@yahoo.com>
2024-12-26 03:13:02 +0100

    authentication request dto

---
+++ src/main/java/gr/aueb/cf/myproject/dto/AuthenticationRequestDTO.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.myproject.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequestDTO {
+
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String password;
+}
