0ceac467a31c651fdb3eb81ebc19bb673e33eb0d
Dimi Syskakis <Syskakisdimitrios@yahoo.com>
2024-12-26 02:01:16 +0100

    Admin

--- src/main/java/gr/aueb/cf/myproject/model/Admin.java
+++ src/main/java/gr/aueb/cf/myproject/model/Admin.java
@@ -1,19 +1,18 @@
 package gr.aueb.cf.myproject.model;
 
-import gr.aueb.cf.myproject.dto.UserInsertDTO;
+
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.UUID;
 
 
 @Entity
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
 @Builder
 @Table(name = "admins")
 public class Admin extends BaseRole {
-    private User user;
+
+
 }
