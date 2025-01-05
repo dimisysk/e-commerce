f8073dfd2524bacfbf308d54e1be9dbf46c08b34
Dimi Syskakis <Syskakisdimitrios@yahoo.com>
2025-01-05 18:43:28 +0100

    customer access denied handler

---
+++ src/main/java/gr/aueb/cf/myproject/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.myproject.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json; charset=utf-8");
+        String json = "{\"code\":\"User not authorized\",\"description\":\"User not authorized to access this resource \"}";
+        response.getWriter().write(json);
+    }
+}
