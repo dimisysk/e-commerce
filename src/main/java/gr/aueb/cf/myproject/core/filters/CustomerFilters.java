a04f42ff9f93932b9029c1b083f6db24ed9a8041
Dimi Syskakis <Syskakisdimitrios@yahoo.com>
2025-01-05 18:44:37 +0100

    customer filters

---
+++ src/main/java/gr/aueb/cf/myproject/core/filters/CustomerFilters.java
@@ -0,0 +1,27 @@
+package gr.aueb.cf.myproject.core.filters;
+
+
+import gr.aueb.cf.myproject.core.enums.GenderType;
+import lombok.*;
+import org.springframework.lang.Nullable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@AllArgsConstructor
+@Builder
+public class CustomerFilters extends GenericFilters{
+    @Nullable
+    private String uuid;
+
+    @Nullable
+    private String userSsn;
+
+    @Nullable
+    private GenderType userGender;
+
+    @Nullable
+    private Boolean active;
+
+}
