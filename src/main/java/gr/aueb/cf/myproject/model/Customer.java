7c8a40315406d6933d9949512ff1993dd69e66e1
Dimi Syskakis <Syskakisdimitrios@yahoo.com>
2025-01-11 00:33:12 +0100

    customer

--- src/main/java/gr/aueb/cf/myproject/model/Customer.java
+++ src/main/java/gr/aueb/cf/myproject/model/Customer.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @Table(name = "customers")
 public class Customer extends BaseRole {
 
-    private Integer discountCardNumber;
+    @NonNull
+    private String discountCardNumber;
 
 }
